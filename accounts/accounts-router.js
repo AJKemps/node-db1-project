9cc61470ea2f1887ac5eb056b0b7c7e2ee009de4
Alex Kemper <alexkemper@MacBook-Pro.local>
2020-08-11 11:11:58 -0400

    get specific id and post new account are both working, moving onto put and delete

--- accounts/accounts-router.js
+++ accounts/accounts-router.js
@@ -16,10 +16,33 @@ router.get("/", (req, res) => {
 });
 
 router.get("/:id", (req, res) => {
+  requestedAccount = req.params.id;
+
+  db.select("*")
+    .from("accounts")
+    .where({ id: requestedAccount })
+    .then((response) => {
+      res.status(200).json({ data: response });
+    })
+    .catch((error) => {
+      res.status(500).json({ error: error });
+    });
+
   // goes here
 });
 
 router.post("/", (req, res) => {
+  let newAccount = req.body;
+
+  db("accounts")
+    .insert(newAccount)
+    .returning("id")
+    .then((response) => {
+      res.status(201).json({ data: response });
+    })
+    .catch((error) => {
+      res.status(500).json({ error: error });
+    });
   // goes here
 });
 
